8e52fc3ddf4d9450244f2cae213e48da10d99c40
jainit <jainit@ecologylab.net>
2012-08-09 11:57:57 +0000

    PUT composition and POST composition with HTTP responses

--- InfoShare/apps/api/utils.py
+++ InfoShare/apps/api/utils.py
@@ -10,5 +10,15 @@ def GetDict(queryobj):
     tags_list = []
     for tag in queryobj.tags.all():
         tags_list.append(tag.tag)
-    info_dict = dict(get_composition_metadata_response = dict(metadata = dict(username = queryobj.user.username,title = queryobj.name,description = queryobj.description, created = GetDateTime(queryobj.added),tags = dict(tag = tags_list))))
+    info_dict = dict(get_composition_metadata_response = dict(metadata = dict(username = queryobj.user.username,title = queryobj.name,description = queryobj.description, created = GetDateTime(queryobj.added),last_modified=GetDateTime(queryobj.last_modified),tags = dict(tag = tags_list))))
     return info_dict
+
+def UserDict(queryobj):
+    tags_list = []
+    for tag in queryobj.tags.all():
+        tags_list.append(tag.tag)
+    info_dict = dict(list_user_composition_response = dict(metadata = dict(hash_key = queryobj.hash_key,title = queryobj.name,description = queryobj.description, created = GetDateTime(queryobj.added),last_modified=GetDateTime(queryobj.last_modified),tags = dict(tag = tags_list))))
+    return info_dict
+
+    
+
