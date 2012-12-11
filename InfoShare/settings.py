5200eb527e7f229061a424c656e0da3f93e27594
Rhema Linder <rhemalinder@gmail.com>
2012-12-11 17:00:02 -0600

    Change default db to be sqlite.

--- InfoShare/settings.py
+++ InfoShare/settings.py
@@ -27,8 +27,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     "default": {
-        "ENGINE": "postgresql_psycopg2", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "newsocial",                       # Or path to database file if using sqlite3.
+        "ENGINE": "sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "/tmp/socialite",                       # Or path to database file if using sqlite3.
         "USER": "",                             # Not used with sqlite3.
         "PASSWORD": "",                         # Not used with sqlite3.
         "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
